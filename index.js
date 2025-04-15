e9475d600ca492fc139cc48bd121ba131d5ff705
vedantv20 <vedantvataresandip112023159@sitrc.org>
2025-04-15 21:30:40 +0530

    add self ping to server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv/config");
 const mineflayer = require("mineflayer");
+const http = require("http");
+const fetch = require("node-fetch");
 
 const bot = mineflayer.createBot({
   host: process.env.host,
@@ -36,12 +38,9 @@ bot.on("kicked", (reason) => {
 function handleSleep() {
   setInterval(() => {
     if (!bot) return;
-
     const dayTime = bot.time.timeOfDay;
 
     if (dayTime >= 13000 && dayTime <= 23000) {
-      console.log("It's night time!");
-
       if (!bot.isSleeping) {
         const bed = bot.findBlock({
           matching: (block) => bot.isABed(block),
@@ -62,8 +61,35 @@ function handleSleep() {
           console.log("No bed found nearby.");
         }
       }
-    } else {
-      console.log("It's not night time.");
     }
   }, 20000);
 }
+
+function startHttpServer() {
+  const PORT = process.env.server_port || 3000;
+
+  http
+    .createServer((req, res) => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Bot is alive!\n");
+    })
+    .listen(PORT, () => {
+      console.log(`HTTP Server running on port ${PORT}`);
+    });
+}
+
+function startSelfPing() {
+  const url = process.env.SELF_URL;
+
+  setInterval(() => {
+    if (url) {
+      fetch(url)
+        .then((res) => console.log(`Self-ping success: ${res.status}`))
+        .catch((err) => console.log(`Self-ping error: ${err.message}`));
+    } else {
+      console.log("SELF_URL not set in environment.");
+    }
+  }, 12 * 60 * 1000);
+}
+startHttpServer();
+startSelfPing();
